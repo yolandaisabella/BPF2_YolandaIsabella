3c664be9013c3b723915b4f58fac52956b9b7d28
yolandaisabella <yolanda24ti@mahasiswa.pcr.ac.id>
2026-04-01 08:38:47 +0700

    P3

---
+++ react-yolanda-2tie/2tie-app/src/pertemuan-3/HelloWorld.jsx
@@ -0,0 +1,47 @@
+export default function HelloWorld(){
+    const propsUserCard = {
+        nama: "Goku",
+        nim: "999999",
+        tanggal: "2025-01-01"
+    }
+
+    return (
+        <div>
+            <img src="img/picture1.png" width="100%"/>
+            <h1>Hello World</h1>
+            <p>Selamat Belajar ReactJs</p>
+            <GreetingBinjai/>
+            <QuoteText/>
+            <UserCard nama = "yolan" nim = "2455301197" tanggal = "2005/11/23"/>
+            <UserCard {...propsUserCard}/>
+        </div>
+    )
+}
+
+function GreetingBinjai(){
+    return (
+        <small>Salam dari Binjai</small>
+    )
+}
+
+function QuoteText() {
+    const text = "Mulutmu Harimaumu";
+    const text2 = "Aku ingin jadi macan";
+    return (
+        <div>
+            <hr/>
+            <p>{text.toLowerCase()}</p>
+            <p>{text2.toUpperCase()}</p>
+        </div>
+    )
+}
+function UserCard(props){
+    return (
+        <div>
+            <hr/>
+            <h3>Nama: {props.nama}</h3>
+            <p>NIM: {props.nim}</p>
+            <p>Tanggal: {props.tanggal}</p>
+        </div>
+    )
+}
